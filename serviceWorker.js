af95512d1c11a008d006af4fc98f4b16895501cf
MoPashtun <speen@stud.fra-uas.de>
2021-11-05 10:29:48 +0100

    test
    a

--- serviceWorker.js
+++ serviceWorker.js
@@ -31,3 +31,55 @@ self.addEventListener("fetch", fetchEvent => {
   );
 });
 
+function download() {
+  console.log('Saving data');
+  theRecorder.stop();
+  theStream.getTracks()[0].stop();
+
+  var blob = new Blob(recordedChunks, {type: "video/webm"});
+  var url = (window.URL || window.webkitURL).createObjectURL(blob);
+  var a = document.createElement("a");
+  document.body.appendChild(a);
+  a.style = "display: none";
+  a.href = url;
+  a.download = 'test.webm';
+  a.click();
+  
+  // setTimeout() here is needed for Firefox.
+  setTimeout(function () {
+      (window.URL || window.webkitURL).revokeObjectURL(url);
+  }, 100); 
+}
+
+function createDB() {
+  idb.open('products', 1, function(upgradeDB) {
+    var store = upgradeDB.createObjectStore('beverages', {
+      keyPath: 'id'
+    });
+    store.put({id: 123, name: 'coke', price: 10.99, quantity: 200});
+    store.put({id: 321, name: 'pepsi', price: 8.99, quantity: 100});
+    store.put({id: 222, name: 'water', price: 11.99, quantity: 300});
+  });
+}
+
+function cacheAssets() {
+  return caches.open('cache-v1')
+  .then(function(cache) {
+    return cache.addAll([
+      '.',
+      'index.html',
+      'styles/main.css',
+      'js/offline.js',
+      'img/coke.jpg'
+    ]);
+  });
+}
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      // Check cache but fall back to network
+      return response || fetch(event.request);
+    })
+  );
+});
