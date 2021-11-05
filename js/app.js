b51af527011b1ba5f09bc53140d99759129f1f8d
MoPashtun <speen@stud.fra-uas.de>
2021-11-05 10:51:30 +0100

    a
    a

--- js/app.js
+++ js/app.js
@@ -164,7 +164,7 @@ function getStream (type) {
     console.log('Saving data');
     theRecorder.stop();
     theStream.getTracks()[0].stop();
-    CacheStorage.getUserMedia();
+    // CacheStorage.getUserMedia();
   
     var blob = new Blob(recordedChunks, {type: "video/webm"});
     var url = (window.URL || window.webkitURL).createObjectURL(blob);
@@ -173,7 +173,7 @@ function getStream (type) {
     a.style = "display: none";
     a.href = url;
     a.download = 'test.webm';
-    a.CacheStorage = 'image.mp4';
+    // a.CacheStorage = 'image.mp4';
     a.click();
     
     // setTimeout() here is needed for Firefox.
